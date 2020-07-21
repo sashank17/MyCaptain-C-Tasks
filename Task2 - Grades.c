2718c5a1f1b6fb59c55733e5f51033ca43d892f9
sashank17 <44266341+sashank17@users.noreply.github.com>
2020-07-21 19:57:45 +0530

    Completed Task2 - Grades

---
+++ Task2 - Grades.c
@@ -0,0 +1,17 @@
+//#include <stdio.h>
+//
+//int main() {
+//    int marks;
+//    printf("Enter your marks out of 100:\n");
+//    scanf("%d", &marks);
+//    if (marks >= 85)
+//        printf("GRADE A");
+//    else if (marks >= 70 && marks < 85)
+//        printf("GRADE B");
+//    else if (marks >= 55 && marks < 70)
+//        printf("GRADE C");
+//    else if (marks >= 40 && marks < 55)
+//        printf("GRADE D");
+//    else
+//        printf("GRADE F");
+//}
